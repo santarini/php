70343b6ded32f07b9cc96da562aad2898d3de52e
Makoa <32736507+santarini@users.noreply.github.com>
2018-12-12 11:48:36 -1000

    Update multiplicationTable.php

--- multiplicationTable.php
+++ multiplicationTable.php
@@ -1,21 +1,23 @@
 <h1>Multiplication Table</h1>
 <table>
         <?php
-            echo '<tr><th>&nbsp;</th>';
-            for($i=1; $i<=12; $i++){
-                for($j=1; $j<=12; $j++){
-                    if($i==1){
-                        echo '<th>' . ($i * $j) . '</th>';
+            for($i=0; $i<=12; $i++){
+                for($j=0; $j<=12; $j++){
+                    if($i==0 && $j==0){
+                        echo '<tr><th>&nbsp;</th>';
                     }
-                    if($i!=1 && $j==1){
-                        echo '<th>' . ($i-1) . '</th>';
+                    if($i==0 && $j>0){
+                        echo '<th>' . ($j) . '</th>';
                     }
-                    if($i>1){
+                    if($i>0 && $j==0){
+                        echo '<th>' . $i . '</th>';
+                    }
+                    if($i>0 && $j>0){
                         echo '<td>' . ($i * $j) . '</td>';
                     }
                     if($j==12){
                         echo '</tr>';
-                    }//end of iff
+                    }
                 } //end of for j
             }//end of for i
         ?>
